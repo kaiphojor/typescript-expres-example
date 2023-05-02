1388dfa435f805d674b21696ed1c2f3e0edfa589
kaiphojor <jihoopark7666@gmail.com>
2023-05-02 15:16:44 +0900

    반환값 변경

--- app.ts
+++ app.ts
@@ -7,6 +7,29 @@ const app = express();
 app.get('/', (req, res) => {
 	res.send('Hello World!!!!!!!!!!!!!!!!!!!!!@#%@%#@%@%');
 });
+app.get('/products', (req, res) => {
+	const products = [
+		{
+			category: 'Fruits', price: '$1', stocked: true, name: 'Apple',
+		},
+		{
+			category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit',
+		},
+		{
+			category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit',
+		},
+		{
+			category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach',
+		},
+		{
+			category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin',
+		},
+		{
+			category: 'Vegetables', price: '$1', stocked: true, name: 'Peas',
+		},
+	];
+	res.send({products});
+});
 
 app.listen(port, () => {
 	console.log(`server running at http://localhost:${port}`);
